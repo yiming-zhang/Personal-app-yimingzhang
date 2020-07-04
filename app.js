2162461001b0d70d64903434cff0c00d1e83c29b
Glitch (tjhickey724-highschoolappdesign-1) <none>
2020-07-04 12:12:18 +0000

    🙇💈 Checkpoint ./app.js:18374370/754

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ app.use(function(err, req, res, next) {
 });
 
 //Here we set the port to use
-const port='5500'
+const port='5000'
 app.set('port', port);
 
 // and now we startup the server listening on that port
@@ -77,9 +77,46 @@ const server = http.createServer(app);
 server.listen(port);
 
 function handleError(e){ console.log("Error starting the server")}
-server.on('error', handleError)
+
 
 function handleListening() { console.log("Server started on port "+port)}
+
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+
+server.on('error',onError)
+
 server.on('listening', handleListening)
 
 module.exports = app;
